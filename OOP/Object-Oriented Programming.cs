a7a77ddf05f75e86e400357fa8156484ed804707
Nowwook <110366684+Nowwook@users.noreply.github.com>
2022-11-08 23:02:42 +0900

    Create Object-Oriented Programming.cs

---
+++ OOP/Object-Oriented Programming.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace CircleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 객체 지향(Object-Oriented)
+            // 객체 지향(OOP) 프로그래밍의 4대 원칙(캡슐화, 추상화, 상속, 다형성)
+        }
+    }
+}
