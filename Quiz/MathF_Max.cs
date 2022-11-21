9a59c956089d96a0e4eb40680bc81371eab0a94d
Nowwook <110366684+Nowwook@users.noreply.github.com>
2022-11-21 23:51:51 +0900

    Update and rename 곱셈 최댓값.cs to MathF_Max.cs

--- Quiz/MathF_Max.cs
+++ Quiz/MathF_Max.cs
@@ -2,11 +2,13 @@ using System;
 
 // 원소 중 두 개를 곱해 만들 수 있는 최댓값
 // [1, 2, -3, 4, -5] >> 15
+
 public class Solution {
     public int solution(int[] numbers)
     {
         int maxLen = numbers.Length-1 ;
         Array.Sort(numbers) ;
+        // MathF.Max  두 개의 수 중 더 큰 숫자를 반환
         return (int)MathF.Max(numbers[0]*numbers[1], numbers[maxLen]*numbers[maxLen-1]) ;
     }
 }
