603a71d53b0755c2b16b21842866c84054d87afb
Nowwook <110366684+Nowwook@users.noreply.github.com>
2023-01-12 01:08:33 +0900

    Create 주석.cs

---
+++ 기초/주석.cs
@@ -0,0 +1,19 @@
+// 한줄
+  ctrl + k + c
+  ctrl + k + u
+  
+/*
+* 여러줄
+* shift + ctrl + /
+*/
+
+/// 자동 완성 주석
+    함수에 대한 요약과 파라미터에 대한 설명
+    마우스 오버로 확인가능
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="k"></param>
+/// <param name="score"></param>
+/// <returns></returns>
