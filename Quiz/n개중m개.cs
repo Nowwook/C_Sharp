f4b2fad1151346f997e87d8c664cde0ce16f3185
Nowwook <110366684+Nowwook@users.noreply.github.com>
2023-02-01 23:59:09 +0900

    Update n개중m개.cs

--- Quiz/n개중m개.cs
+++ Quiz/n개중m개.cs
@@ -18,3 +18,59 @@ public class Solution {
         return temp;
     }
 }
+
+// 재귀사용
+using System;
+using System.Numerics;
+
+public class Solution 
+{
+    public int solution(int balls, int share)
+    {
+        return Combination(balls, share);
+    }
+
+    public int Combination(int n, int m)
+    {
+        if (m == 0 || n == m) 
+        {
+            return 1;
+        }
+
+        return Combination(n - 1, m - 1) + Combination(n - 1, m);
+    }
+}
+
+using System;
+
+public class Solution {
+    public long solution(long balls, long share) {
+        long answer = 0;
+        long a = balls-share;
+        long e =0;
+        long d=1;
+        long f= 1;
+        if( a>share)
+        {
+            e=share;
+        }
+        else
+        {
+            e=a;
+        }
+        if(balls==share)
+        {
+            answer=1;
+        }
+        else
+        {
+            for(long i =0; i < e; i++)
+            {
+                d = d*(balls-i)/(i+1);
+            }
+            answer= d;
+
+        }
+        return answer;
+    }
+}
